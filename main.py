56ee74e89ca6d94a2415f82988075122cd300536
miguelpiress21 <ajudamig@gmail.com>
2026-01-08 21:32:21 -0300

    Update main.py
    adicionei o token

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 from bot_multimodal import BotMultimodal
 
-TOKEN = os.getenv("TELEGRAM_TOKEN")
+TOKEN = os.getenv("8479058192:AAHqYwqvh5dqNmnhl9YToT9_rMZtfXrywKY")
 
 bot = BotMultimodal(TOKEN)
+
 bot.run()
